767395307ce3e3f835ec0e968771a033b5adb82b
assignment-01 <ykhan9463@gmail.com>
2025-08-31 20:13:18 +0600

    Display Saved cart from Local storage

--- src/Components/Bottles.jsx
+++ src/Components/Bottles.jsx
@@ -1,33 +1,48 @@
 import { useEffect, useState } from "react";
 import Bottle from "./Bottle";
 import { addToLS, getStoredCart } from "./LocalStorage";
+import Cart from "./Cart";
 
 const Bottles = () => {
   const [bottles, setBottles] = useState([]);
   const [cart, setCart] = useState([]); // cart state
+  const savedCart = []; // temporary array to hold saved cart items
 
   useEffect(() => {
     fetch("bottles.json")
       .then((res) => res.json())
       .then((data) => setBottles(data));
   }, []);
+
   // load cart from local storage on component mount
   useEffect(() => {
     console.log(bottles.length);
-    if (bottles.length === 0) {
+    if (bottles.length > 0) {
       const storedCart = getStoredCart();
-      console.log(storedCart);
+      console.log("Stored cart:", storedCart);
+      console.log("Fetched bottles:", bottles);
+
+      for (const id of storedCart) {
+        const bottle = bottles.find((bottle) => bottle.id === id);
+        if (bottle) {
+          savedCart.push(bottle); // add bottle to cart state
+        }
+      }
+      console.log("savedCart", savedCart);
+      setCart(savedCart); // update cart state
     }
-  }, [bottles]);
+  }, [bottles]); // <-- depends on bottles
+
   const handleAddToCart = (bottle) => {
     const newCart = [...cart, bottle];
     setCart(newCart);
     addToLS(bottle.id);
   };
+
   return (
     <div>
       <h2>Bottles Available: {bottles.length}</h2>
-      <h4>Cart: {cart.length}</h4>
+      <Cart cart={cart}></Cart>
       <div
         className="bottles-container"
         style={{
---
+++ src/Components/Cart.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./cart.css";
+
+const Cart = ({ cart }) => {
+  return (
+    <div>
+      <h4>Cart: {cart.length}</h4>
+      <div className="cart-container">
+        {cart.map((bottle) => (
+          <img src={bottle.img} alt="" />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
